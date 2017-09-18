5e5778cb125c75f9ad59042ba5e89bc6fa6b719e
glwz2017 <2991703821@qq.com>
2017-09-18 19:02:56 +0800

    英语交互网站

---
+++ english-online/js/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2017/9/13 0013.
+ */
+jQuery(function(){
+//获取登陆按钮
+jQuery('.login').on('click',function(){
+	jQuery('#sign').modal('show');
+});
+//获取更多栏目
+	jQuery('.more-message').on('click',function(){
+		$('.sd').attr('data-toggle','dropdown');
+	});
+
+
+
+
+
+
+
+
+
+
+
+});//函数结尾
